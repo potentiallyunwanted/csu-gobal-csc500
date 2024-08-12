e06a57d75f935383b73939ed07bf7bb37eacfbc3
Quoc Duong <quoc.duong@potentiallyunwanted.com>
2024-08-12 12:08:12 -0400

    init

---
+++ Critical Thinking Assignment/1/add_sub.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+number1 = input('Number 1: ')
+number2 = input('Number 2: ')
+print('Sum:', int(number1) + int(number2))
+print('Difference:', int(number1) - int(number2))
---
+++ Critical Thinking Assignment/1/multiply_divide.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+number1 = input('Number 1: ')
+number2 = input('Number 2: ')
+print('Factor:', int(number1) * int(number2))
+print('Quotient:', int(number1) / int(number2))
